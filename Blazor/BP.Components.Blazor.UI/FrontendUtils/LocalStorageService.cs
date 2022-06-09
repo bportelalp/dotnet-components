56f61c70c3a19a38ac52531b5e8f1b29a2cbed3e
bportela <bportela.dev@gmail.com>
2022-06-09 22:22:16 +0200

    Componente popup para mensajes de error o éxito con sweetalert

--- Blazor/BP.Components.Blazor.UI/FrontendUtils/LocalStorageService.cs
+++ Blazor/BP.Components.Blazor.UI/FrontendUtils/LocalStorageService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BP.Components.Blazor.UI.Services
+namespace BP.Components.Blazor.UI.FrontendUtils
 {
     public class LocalStorageService
     {
---
+++ Blazor/BP.Components.Blazor.UI/FrontendUtils/PopUp.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP.Components.Blazor.UI.FrontendUtils
+{
+    /// <summary>
+    /// Incluir librería 
+    ///<script src = "https://cdn.jsdelivr.net/npm/sweetalert2@8" ></ script >
+    /// </summary>
+    public class PopUp
+    {
+        private readonly IJSRuntime js;
+
+        public PopUp(IJSRuntime js)
+        {
+            this.js = js;
+        }
+
+        public async Task ShowError(string mensaje)
+        {
+            await ShowMessage("Error", mensaje, "error");
+        }
+
+        public async Task ShowSucess(string mensaje)
+        {
+            await ShowMessage("Éxito", mensaje, "success");
+        }
+
+        private async ValueTask ShowMessage(string titulo, string mensaje, string tipoMensaje)
+        {
+            await js.InvokeVoidAsync("Swal.fire", titulo, mensaje, tipoMensaje);
+        }
+    }
+}
