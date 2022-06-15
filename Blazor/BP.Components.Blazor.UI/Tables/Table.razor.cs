bea721c36cf8ab33826f6bcdb9ade97b209a782e
bportela <bportela.dev@gmail.com>
2022-06-15 19:27:17 +0200

    Creando un grid basado en el de meizou

---
+++ Blazor/BP.Components.Blazor.UI/Tables/Table.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP.Components.Blazor.UI.Tables
+{
+    public partial class Table<TRow> : ComponentBase
+    {
+        [Parameter] public IEnumerable<TRow> Data { get; set; }
+        [Parameter] public string TableCSS { get; set; } = "table table-hover";
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
