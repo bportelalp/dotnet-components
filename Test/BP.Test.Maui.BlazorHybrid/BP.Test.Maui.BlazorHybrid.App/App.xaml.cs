453b095aceb40d8b9ec12a22c743d57fab5a0486
bportela <bportela.dev@gmail.com>
2022-08-15 20:22:04 +0200

    Inicio pruebas MAUI y blazor

---
+++ Test/BP.Test.Maui.BlazorHybrid/BP.Test.Maui.BlazorHybrid.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BP.Test.Maui.BlazorHybrid.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Test/BP.Test.Maui.BlazorHybrid/BP.Test.Maui.BlazorHybrid.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BP.Test.Maui.BlazorHybrid.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
